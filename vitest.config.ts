725d68777ce7212c22ffd4e4158f575fa5e64450
Vincent Leighton <vincentleighton@gmail.com>
2025-05-11 16:17:07 -0700

    feat: add coverage reporting configuration for Vitest and update test script

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,22 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        all: true,
+        include: ['src/**/*.{js,ts,vue}'],
+        exclude: [
+          'src/main.ts',
+          'src/router/index.ts',
+          'src/types/**/*.ts',
+          'src/views/__tests__/*',
+          'src/components/__tests__/*',
+          'src/assets/**/*',
+          'src/App.vue',
+          'src/components/**/*',
+          'src/views/AboutView.vue',
+        ],
+      },
     },
   }),
 )
